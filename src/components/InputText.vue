1bd634c415e49f9b17b205ae422e01d61a299cbd
Yongsoo Choi <curly.yongsoo@gmail.com>
2024-12-22 22:36:00 +0900

    feat: InputText 컴포넌트에 입력 필드 초기화 기능 추가

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -2,18 +2,32 @@
 type InputTextProps = {
   label: string;
 };
-const model = defineModel<string>();
+
 defineProps<InputTextProps>();
+
+const model = defineModel<string>();
+
+const clearInput = (event: MouseEvent) => {
+  model.value = "";
+};
 </script>
 
 <template>
-  <label class="flex flex-col gap-4">
+  <label class="flex flex-col gap-4 relative">
     {{ label }}
     <input
-      class="border-gray-400 border rounded-md h-10 px-4 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none"
+      class="border-gray-400 border rounded-md h-10 pl-4 pr-8 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none"
       type="text"
       autocomplete="off"
       v-model="model"
     />
+    <button
+      v-if="model && model.length > 0"
+      type="button"
+      class="absolute right-4 top-3/4 transform -translate-y-1/2 cursor-pointer"
+      @click="clearInput"
+    >
+      X
+    </button>
   </label>
 </template>
